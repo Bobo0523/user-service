b33186542707e142df274ab356be32b813b11d07
Bobo0523 <zbx0523@gmail.com>
2026-09-03 01:18:17 +1200

    added jwtdecoder

--- src/main/java/org/example/config/SecurityConfig.java
+++ src/main/java/org/example/config/SecurityConfig.java
@@ -1,15 +1,26 @@
 package org.example.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    @Value("${COGNITO_URI}")
+    private String cognitoUri;
 
+    // 手动创建一个 JwtDecoder Bean，Spring Security 看到它就不会报错了
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        // 明确告诉 Spring 去这个 Cognito 地址下载公钥进行 JWT 验证
+        return JwtDecoders.fromIssuerLocation(cognitoUri);
+    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
