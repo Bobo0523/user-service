a588e6ac5ed505a62ae249c199d1c82748b07904
Bobo0523 <zbx0523@gmail.com>
2026-09-09 10:25:07 +1200

    delete test

--- src/main/java/org/example/Controller/test.java
+++ src/main/java/org/example/Controller/test.java
@@ -1,21 +1,21 @@
-package org.example.Controller;
-
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
-
-@RestController
-public class test {
-
-    @GetMapping("/api/public")
-    public ResponseEntity<Map<String, String>> testPublicApi() {
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "Hello from Spring Boot on EC2!");
-        response.put("status", "success");
-        return ResponseEntity.ok(response);
-    }
-}
+//package org.example.Controller;
+//
+//
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@RestController
+//public class test {
+//
+//    @GetMapping("/api/public")
+//    public ResponseEntity<Map<String, String>> testPublicApi() {
+//        Map<String, String> response = new HashMap<>();
+//        response.put("message", "Hello from Spring Boot on EC2!");
+//        response.put("status", "success");
+//        return ResponseEntity.ok(response);
+//    }
+//}
